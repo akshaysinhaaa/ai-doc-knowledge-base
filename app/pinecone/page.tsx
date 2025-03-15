aef522223ad08f8e0dde0eb5ab33524749c3eb7a
Akshay Sinha <e22cseu0385@bennett.edu.in>
2025-03-15 22:14:59 +0530

    initial commit

---
+++ app/pinecone/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type Props = {}
+
+const VectorDBpage = (props: Props) => {
+  return (
+    <main className='flex flex-col items-center p-24'>
+
+    </main>
+  )
+}
+
+export default VectorDBpage
