c3d6bf12f3e2cebb0110354988f8abee505f33f4
Akshay Sinha <e22cseu0385@bennett.edu.in>
2025-03-15 22:19:01 +0530

    initial commit

--- app/pinecone/page.tsx
+++ app/pinecone/page.tsx
@@ -1,3 +1,4 @@
+import { Card, CardContent, CardHeader } from '@/components/ui/card'
 import React from 'react'
 
 type Props = {}
@@ -5,7 +6,10 @@ type Props = {}
 const VectorDBpage = (props: Props) => {
   return (
     <main className='flex flex-col items-center p-24'>
-
+        <Card>
+            <CardHeader></CardHeader>
+            <CardContent></CardContent>
+        </Card>
     </main>
   )
 }
