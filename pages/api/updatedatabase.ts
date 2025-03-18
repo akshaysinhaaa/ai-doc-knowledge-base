93b394ee529d0478f27669ef486779c6f6e82b60
Akshay Sinha <e22cseu0385@bennett.edu.in>
2025-03-18 23:16:42 +0530

    langchain database

--- pages/api/updatedatabase.ts
+++ pages/api/updatedatabase.ts
@@ -1,6 +1,7 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { DirectoryLoader } from 'langchain/document_loaders/fs/directory';
 import { PDFLoader } from 'langchain/document_loaders/fs/pdf';
+import { TextLoader } from 'langchain/document_loaders/fs/text';
 
 export default async (req:NextApiRequest, res:NextApiResponse ) => {
     if(req.method === 'POST') {
@@ -9,8 +10,12 @@ export default async (req:NextApiRequest, res:NextApiResponse ) => {
     }
 }
 
-function handleUpload(indexname: string, namespace: string, res: NextApiResponse) {
+async function handleUpload(indexname: string, namespace: string, res: NextApiResponse) {
     const loader = new DirectoryLoader('./documents', {
-        '.pdf': (path: string) => new PDFLoader(path)
-    })
+        '.pdf': (path: string) => new PDFLoader(path, {
+            splitPages: false
+        }),
+        '.txt': (path: string) => new TextLoader(path)
+    });
+    const docs = await loader.load();
 }
