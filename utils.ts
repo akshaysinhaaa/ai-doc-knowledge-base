f78c9973b88658d2e9cabd57665a0368ad4e417e
Akshay Sinha <e22cseu0385@bennett.edu.in>
2025-03-19 22:54:29 +0530

    langchain database

--- utils.ts
+++ utils.ts
@@ -1,10 +1,12 @@
 import { Pinecone } from "@pinecone-database/pinecone";
+import { Document } from "langchain/document";
 
 export async function updateVectorDB(
     client: Pinecone,
     indexname: string,
     namespace: string,
-    docs: Document[]
+    docs: Document[],
+    progressCallback: (fileName: string, totalChunks: number, chunksUpserted: number, isComplete: boolean) 
 
 ) {
     
